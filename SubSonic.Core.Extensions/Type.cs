6fcffa5de1a050e7f152435b3f4a40d1c201bb3f
Kenneth Carter <kccarter32@gmail.com>
2020-08-04 14:55:23 -0600

    no message

---
+++ SubSonic.Core.Extensions/Type.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace SubSonic.Core
+{
+    public static partial class Extensions
+    {
+        public static string ToConfigName(this Type type)
+        {
+            // Do not qualify types from mscorlib/System.Private.CoreLib otherwise calling between process running with different frameworks won't work
+            // i.e. "System.String, mscorlib" (.NET FW) != "System.String, System.Private.CoreLib" (.NET CORE)
+            if (type.Assembly.GetName().Name == "mscorlib" ||
+                type.Assembly.GetName().Name == "System.Private.CoreLib")
+                return type.FullName;
+
+            var name = type.AssemblyQualifiedName;
+            name = Regex.Replace(name, @", Version=\d+.\d+.\d+.\d+", string.Empty);
+            name = Regex.Replace(name, @", Culture=\w+", string.Empty);
+            name = Regex.Replace(name, @", PublicKeyToken=\w+", string.Empty);
+            return name;
+        }
+    }
+}
